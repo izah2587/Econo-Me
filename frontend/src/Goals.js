a027cbef1f9f4a1b45589efa0aa4d44074b7059e
Muhammad Hamza Arshad Majeed <hamzamajeed@10-20-207-164.dynapool.wireless.nyu.edu>
2024-12-11 18:20:24 -0500

    Added Goals.css

--- frontend/src/Goals.js
+++ frontend/src/Goals.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
-
+import './Goals.css'
 const Goals = () => {
   const { getAccessTokenSilently } = useAuth0();
   const [goals, setGoals] = useState({ active: [], completed: [] });
